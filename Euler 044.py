634fc317d0e3a1f1cdc91568fb62229001348c39
Stephen Roche <stephen.roche00@gmail.com>
2020-01-24 13:15:34 +1100

    Update Euler 044.py

--- Euler 044.py
+++ Euler 044.py
@@ -30,3 +30,21 @@ def pentagonal(n):
 
 # print(31+28+25+22+19+16+13, 34+37+40+43)
 
+import math
+
+pentagonals = [0, 1]
+k = 1
+min_D = math.inf
+while k < len(pentagonals) and k < 1000:
+	while pentagonals[-1] < 2 * pentagonals[k]:
+		pentagonals += [pentagonal(len(pentagonals))]
+	# print(pentagonals[k], pentagonals)
+
+	i = 1
+	while i < k and pentagonals[i] < min_D:
+		if (pentagonals[k] - pentagonals[i]) in pentagonals and (2 * pentagonals[k] - pentagonals[i]) in pentagonals:
+			print('%d - %d = %d, %d + %d = %d', pentagonals[k], pentagonals[k] - pentagonals[i], pentagonals[i], pentagonals[k], pentagonals[k] - pentagonals[i], 2 * pentagonals[k] - pentagonals[i])
+			min_D = pentagonals[i]
+		i += 1
+
+	k += 1
